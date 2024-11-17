f15b7151885b1fefc9a4eb1f642d2bb868ff2500
Ayush Singh <ayushkssk@gmail.com>
2024-11-17 16:37:44 +0530

    update

--- autologin.py
+++ autologin.py
@@ -1,19 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from getpass import getpass
 
 # username = input("Your Email")
 # password = getpass("Your Password")
-driver = webdriver.Chrome("C:\\webdriver\\chromedriver.exe")
-# driver.get("https://www.facebook.com/")
+driver = webdriver.Safari()
 driver.get("https://webmail1.hostinger.in/?_task=mail&_mbox=INBOX")
-# username_textbox = driver.find_element_by_id("email")
-username_textbox = driver.find_element_by_id("rcmloginuser")
-# username_textbox.send_keys(username)
+
+username_textbox = driver.find_element(By.ID, "rcmloginuser")
 username_textbox.send_keys("ayush.kssk@devwings.in")
 
-password_textbox = driver.find_element_by_id("rcmloginpwd")
-#password_textbox.send_keys(password)
+password_textbox = driver.find_element(By.ID, "rcmloginpwd")
 password_textbox.send_keys("Devwings*0987")
 
-login_button = driver.find_element_by_id("rcmloginsubmit")
-login_button.submit()
+login_button = driver.find_element(By.ID, "rcmloginsubmit")
+login_button.click()
